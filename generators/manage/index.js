052cf594ff03e207ece31e3f98095b907ef3be2c
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2021-03-04 18:20:37 +0500

    Generate empty model

--- generators/manage/index.js
+++ generators/manage/index.js
@@ -1,14 +1,34 @@
-'use strict';
-const Generator = require('yeoman-generator');
+"use strict";
+const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
 
-    this.argument("manage", { type: Array, required: true });
+    this.option("default", {
+      desc: "Set value as default",
+      required: false,
+      default: false
+    });
 
+    this.argument("manage", {
+      type: Array,
+      required: true,
+      desc: "Parameters to pass to manage.py"
+    });
   }
+
+  configuring() {
+    if (this.options.manage[0] === "startapp") {
+      if (this.options.default) {
+        this.config.set("defaultApp", this.options.manage[1]);
+      }
+    }
+  }
+
   writing() {
-    this.spawnCommand('python', ['manage.py', ...this.options.manage], { cwd: this.destinationPath('') });
+    this.spawnCommand("python", ["manage.py", ...this.options.manage], {
+      cwd: this.destinationPath("")
+    });
   }
 };
--- generators/model/index.js
+++ generators/model/index.js
@@ -1,4 +1,63 @@
 "use strict";
+const _ = require("lodash");
+const ejs = require("ejs");
+
 const Generator = require("yeoman-generator");
 
-module.exports = class extends Generator {};
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.option("defaultApp", {
+      desc: "Name of app to add models in",
+      type: String,
+      default: this.config.get("defaultApp"),
+      required: false
+    });
+    this.props = _.merge(
+      {},
+      this.config.getAll(),
+      _.pick(this.options, "defaultApp")
+    );
+  }
+
+  prompting() {
+    const prompts = [];
+    if (_.isEmpty(this.props.app)) {
+      prompts.push({
+        type: "input",
+        name: "config.defaultApp",
+        message: "Enter app name to add models in?"
+      });
+    }
+
+    _.merge(prompts, [
+      {
+        type: "input",
+        name: "model.name",
+        message: "Enter name of the model?"
+      }
+    ]);
+
+    if (!_.isEmpty(prompts)) {
+      return this.prompt(prompts).then(props => {
+        _.merge(this.props, props.config);
+        this.model = props.model;
+        // This.log(this.props);
+      });
+    }
+  }
+
+  writing() {
+    // This.log(this.model);
+    this.log(this.templatePath("model.py.ejs"));
+    const modelCode = ejs.render(
+      this.fs.read(this.templatePath("model.py.ejs")),
+      this.model
+    );
+    this.fs.append(
+      this.destinationPath(`${this.props.defaultApp}/models.py`),
+      modelCode
+    );
+  }
+};
